18cc8e81157c1aa3fa1eae1a752e287e6eacf7a9
Kevin Ryan <kevinryan1973@gmail.com>
2023-07-03 12:29:38 +0000

    add tdd langchain query

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # gai-demo
 Generative AI Demo Project
 
-https://vitalflux.com/langchain-chatgpt-hello-world-python-example/
-
-
--- tdd_langchain.py
+++ tdd_langchain.py
@@ -1,3 +1,3 @@
 from langchain.llms import OpenAI
 llm=OpenAI(model_name="text-davinci-003")
-llm("Explain machine learning in one paragraph")
+print(llm("Explain TDD in one paragraph"))
