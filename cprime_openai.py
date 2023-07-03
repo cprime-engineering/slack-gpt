48cf4451e3295435a030f4ea20089563ffc8a898
Kevin Ryan <kevinryan1973@gmail.com>
2023-07-03 12:53:13 +0000

    add llm chain example

--- cprime_openai.py
+++ cprime_openai.py
@@ -3,7 +3,7 @@ import openai
 openai.api_key = os.getenv('OPENAI_API_KEY')
 completion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo", 
-  messages = [{"role": "system", "content" : "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.\nKnowledge cutoff: 2021-09-01\nCurrent date: 2023-03-02"},
+  messages = [{"role": "system", "content" : "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible."},
 {"role": "user", "content" : "Can you describe what the company Cprime does?"}]
 )
 print(completion)
---
+++ llm_chain.py
@@ -0,0 +1,30 @@
+# original code sample: https://vitalflux.com/llm-chain-openai-chatgpt-python-example/
+# requires environment variable "OPENAI_API_KEY"
+
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+ 
+# Step 1: Import the necessary modules
+import os
+ 
+ 
+# Step 2: Get user input
+user_input = input("Enter a concept: ")
+ 
+# Step 3: Define the Prompt Template
+prompt = PromptTemplate(
+    input_variables=["concept"],
+    template="Define {concept} with a real-world example?",
+)
+ 
+# Step 4: Print the Prompt Template
+print(prompt.format(concept=user_input))
+ 
+# Step 5: Instantiate the LLMChain
+llm = OpenAI(temperature=0.9)
+chain = LLMChain(llm=llm, prompt=prompt)
+ 
+# Step 6: Run the LLMChain
+output = chain.run(user_input)
+print(output)
