ca31c083db351abc692d719a654f93f010a72caa
Kevin Ryan <kevinryan1973@gmail.com>
2023-07-05 12:29:26 +0000

    update go installs

---
+++ .devcontainer/build_scripts/tar_installs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install golang ----------------------------------------------------------------------------------------------
+cd /tmp
+wget https://go.dev/dl/go1.17.5.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
+cd /
