a5201356867ceeb03a00d9eff41f5eff83b72185
Kevin Ryan <kevinryan1973@gmail.com>
2023-07-18 21:04:00 +0000

    first working version of retrieval augmented generation integrating pinecone vector database

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Generative AI Copilots
 https://python.langchain.com/
 
 https://python.langchain.com/docs/modules/chains/popular/chat_vector_db
+
+https://www.pinecone.io/learn/series/langchain/langchain-retrieval-augmentation/
--- personas/ada/slackbot.py
+++ personas/ada/slackbot.py
@@ -4,6 +4,9 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 from langchain import OpenAI, LLMChain
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+
 from domain.prompt_engineering.prompt_generator import PromptGenerator
 from domain.slack_integration.web_client import WebClient
 from domain.pinecone_integration.pinecone_client import PineconeClient
@@ -11,44 +14,35 @@ from domain.pinecone_integration.pinecone_client import PineconeClient
 # Set Slack API credentials
 SLACK_BOT_TOKEN = os.environ["ADA_SLACK_BOT_TOKEN"]
 SLACK_APP_TOKEN = os.environ["ADA_SLACK_APP_TOKEN"]
-OPEN_API_KEY = os.environ["CPRIME_OPENAI_API_KEY"]
+OPENAI_API_KEY = os.environ["CPRIME_OPENAI_API_KEY"]
 
 # Initializes app with your bot token
 app = App(token=SLACK_BOT_TOKEN)
 
-# Persona version number
-persona_version_number = "v0.0.0"
-
-# Path to pre prompt templates
-pre_prompt_templates_path = "personas/ada/pre_prompts/"
-
-
-# LLMChain to handle conversations
-chatgpt_chain = LLMChain(
-    llm=OpenAI(openai_api_key=OPEN_API_KEY, temperature=0),
-    prompt=PromptGenerator(
-        pre_prompt_templates_path=pre_prompt_templates_path
-    ).default_template(),
-    verbose=True,
-    memory=ConversationBufferWindowMemory(k=2),
-)
 
 # Store documents and embeddings in the pinecone vectorstore.
 vectorstore = PineconeClient.vectorstore()
+
+# completion llm
+llm = ChatOpenAI(
+    openai_api_key=OPENAI_API_KEY,
+    model_name='gpt-3.5-turbo',
+    temperature=0.0
+)
+
+# retrieval qa to instruct our completion llm to base answer on the information returned from our vector store
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore.as_retriever()
+)
 
 
 # Message handler for Slack
 @app.message(".*")
 def message_handler(message, say, logger):
-    print(message)
-    human_name = WebClient().get_username_from_message(message)
-    chatgpt_chain.prompt = PromptGenerator(
-        pre_prompt_templates_path=pre_prompt_templates_path
-    ).personalized_template(
-        human_name=human_name, version_number=persona_version_number
-    )
-    output = chatgpt_chain.predict(human_input=message["text"])
-    say(output)
+    query = message["text"]
+    say(qa.run(query))
 
 
 # Start app
