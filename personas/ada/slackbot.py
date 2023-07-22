19ae2f2cab8ffb3c451f1343d6723c1f7762811f
Kevin Ryan <kevinryan1973@gmail.com>
2023-07-22 10:09:23 +0000

    hacked first version of custom template to conversational retrieval chain

--- personas/ada/slackbot.py
+++ personas/ada/slackbot.py
@@ -5,6 +5,8 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
+from langchain import PromptTemplate
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 
 from domain.pinecone_integration.pinecone_client import PineconeClient
 
@@ -19,17 +21,34 @@ app = App(token=SLACK_BOT_TOKEN)
 # Store documents and embeddings in the pinecone vectorstore.
 vectorstore = PineconeClient.vectorstore()
 
+
 # completion llm
 llm = ChatOpenAI(
     openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo", temperature=0.0
 )
 
+prompt_template = """Your name is Ada. Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+{context}
+
+Question: {question}
+Helpful Answer:"""
+
+qa_prompt = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+
 # memory
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
-qa = ConversationalRetrievalChain.from_llm(llm, vectorstore.as_retriever(), memory=memory, verbose=True)
-
-
+qa = ConversationalRetrievalChain.from_llm(
+    llm, 
+    vectorstore.as_retriever(), 
+    verbose=True,
+    memory=memory, 
+    combine_docs_chain_kwargs={'prompt': qa_prompt}
+    )
 
 # Message handler for Slack
 @app.message(".*")
